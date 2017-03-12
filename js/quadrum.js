00f1d29646581f9c7e25b61072b83544a98c86fd
Абросимов Алексей < >
2017-03-12 21:24:30 +0300

    works

--- js/quadrum.js
+++ js/quadrum.js
@@ -1,21 +1,37 @@
-$('nav').load('components/nav.html',null,function(){
-     $('a.page-scroll').bind('click', function(event) {
-        event.preventDefault();
-        var $anchor = $(this);
-        $('html, body').stop().animate({
-            scrollTop: $($anchor.attr('href')).offset().top
-        }, 1500, 'easeInOutExpo');
-       
+function initMain(){
+    $('nav').load('components/nav.html',null,function(){
+         $('a.page-scroll').bind('click', function(event) {
+            event.preventDefault();
+            var $anchor = $(this);
+            $('html, body').stop().animate({
+                scrollTop: $($anchor.attr('href')).offset().top
+            }, 1500, 'easeInOutExpo');
+           
+        });
     });
-});
-$('header').load('components/intro.html');
-$('#about').load('components/about.html');
-$('#portfolio').load('components/portfolio.html');
-$('#service').load('components/service.html');
-$('#service-mobile').load('components/service-mobile.html');
-$('#contact').load('components/contact1.html');
-$('#partners').load('components/partners1.html');
-$('footer').load('components/footer.html');
+    $('header').load('components/intro.html');
+    $('#about').load('components/about.html');
+    $('#portfolio').load('components/portfolio.html');
+    $('#service').load('components/service.html');
+    $('#service-mobile').load('components/service-mobile.html');
+    $('#contact').load('components/contact1.html');
+    $('#partners').load('components/partners1.html');
+    $('footer').load('components/footer.html');
+}
+
+function initProject(){
+     $('nav').load('components/inner-nav.html');
+    $("#project").load('components/project-1.html',function(){
+        var carouselSlider = new $.carouselSlider( $('.detailSlider')); 
+    });
+     $('footer').load('components/footer.html');    
+}
+
+function initTextPage(){
+    $('nav').load('components/inner-nav.html');
+    $("#design").load('components/design-interior.html');
+    $('footer').load('components/footer.html');    
+}
 
 function collapseNavbar() {
     if ($(".navbar").offset().top > 50) {
